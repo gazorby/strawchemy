d4b1793bd71ad7abee3d35619508953e1efc355e
gazorby <gazorby@pm.me>
2025-02-26 15:16:39 +0100

    refactor(dto): expose factory instance in pydantic_sqlalchemy.py

--- src/strawchemy/dto/factories/pydantic_sqlalchemy.py
+++ src/strawchemy/dto/factories/pydantic_sqlalchemy.py
@@ -7,9 +7,10 @@ from strawchemy.dto.backend.pydantic import MappedPydanticDTO, PydanticDTOBacken
 from strawchemy.dto.base import DTOFactory
 from strawchemy.dto.inspectors.sqlalchemy import SQLAlchemyInspector
 
-__all__ = ("pydantic_dto",)
+__all__ = ("factory", "pydantic_dto")
 
 _inspector = SQLAlchemyInspector()
 _TypedFactory = DTOFactory[DeclarativeBase, QueryableAttribute[Any], MappedPydanticDTO[Any]]
 
-pydantic_dto = _TypedFactory(_inspector, PydanticDTOBackend(MappedPydanticDTO)).decorator
+factory = _TypedFactory(_inspector, PydanticDTOBackend(MappedPydanticDTO))
+pydantic_dto = factory.decorator
