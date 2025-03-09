43a0308697661080c2b30758ec40eef178ac9f49
gazorby <gazorby@pm.me>
2025-03-09 18:18:50 +0100

    ci(test): call mise task to generate test matrix

--- noxfile.py
+++ noxfile.py
@@ -40,30 +40,3 @@ def unit_tests(session: Session) -> None:
     )
     test_files: list[str] = session.posargs or []
     session.run("pytest", *COMMON_PYTEST_OPTIONS, "-vv", *test_files)
-
-
-@nox.session(name="pyright", python=DEFAULT_PYTHON_VERSION, tags=["lint"])
-def pyright(session: Session) -> None:
-    session.run_install(
-        "uv",
-        "sync",
-        "--all-extras",
-        "--group=lint",
-        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
-    )
-    env = {"PYRIGHT_PYTHON_PYLANCE_VERSION": PYRIGHT_PYTHON_PYLANCE_VERSION}
-    if PYRIGHT_PYTHON_FORCE_VERSION:
-        env["PYRIGHT_PYTHON_FORCE_VERSION"] = PYRIGHT_PYTHON_FORCE_VERSION
-    session.run("pyright", "--version", external=True, env=env)
-    session.run("pyright", *session.posargs, external=True, env=env)
-
-
-@nox.session(name="vulture", python=DEFAULT_PYTHON_VERSION, tags=["lint"])
-def vulture(session: Session) -> None:
-    session.run_install(
-        "uv",
-        "sync",
-        "--group=lint",
-        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
-    )
-    session.run("vulture", "src/", "--min-confidence=100", "--sort-by-size", external=True)
