b7663745ab49f6415ccc1277421559945f7fcfb0
gazorby <gazorby@pm.me>
2025-03-19 14:02:12 +0100

    refactor: remove the default strawchemy instance

--- src/strawchemy/__init__.py
+++ src/strawchemy/__init__.py
@@ -7,7 +7,4 @@ from .sqlalchemy.hook import QueryHook
 from .strawberry import ModelInstance
 from .strawberry.repository import StrawchemyAsyncRepository, StrawchemySyncRepository
 
-__all__ = ("ModelInstance", "QueryHook", "StrawchemyAsyncRepository", "StrawchemySyncRepository", "strawchemy")
-
-
-strawchemy = Strawchemy()
+__all__ = ("ModelInstance", "QueryHook", "Strawchemy", "StrawchemyAsyncRepository", "StrawchemySyncRepository")
