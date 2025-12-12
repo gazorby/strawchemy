a9bb7df85347b4cf9a2afcac33e6fdb2569ea26c
gazorby <gazorby@pm.me>
2025-12-12 23:14:42 +0100

    test(nox): remove 3.9 from matrix

--- noxfile.py
+++ noxfile.py
@@ -9,7 +9,7 @@ from nox_uv import session
 if TYPE_CHECKING:
     from nox import Session
 
-SUPPORTED_PYTHON_VERSIONS = ["3.9", "3.10", "3.11", "3.12", "3.13"]
+SUPPORTED_PYTHON_VERSIONS = ["3.10", "3.11", "3.12", "3.13"]
 COMMON_PYTEST_OPTIONS = ["-n=2", "--showlocals", "-vv"]
 
 here = Path(__file__).parent
